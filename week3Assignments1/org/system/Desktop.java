000485f26877791f5785b4c81374d51b5742ce1d
UshaChinnadurai <93442822+UshaChinnadurai@users.noreply.github.com>
2021-11-13 20:53:39 +0530

    Add files via upload

---
+++ week3Assignments1/org/system/Desktop.java
@@ -0,0 +1,14 @@
+package week3Assignments1.org.system;
+
+public class Desktop extends Computer{
+public void desktopSize() {
+	System.out.println("Desktop Size 11");
+	}
+	public static void main(String[] args) {
+		
+Desktop single=new Desktop();
+single.computerModel();
+single.desktopSize();
+	}
+
+}
